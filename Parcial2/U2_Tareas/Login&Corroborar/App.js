656f8758b49a5829e35b3855a105ee0da0d65d73
Fernando-Angel <ferrando_angel@hotmail.com>
2021-04-25 01:13:27 -0500

    LogIn

--- Parcial2/U2_Tareas/Login&Corroborar/App.js
+++ Parcial2/U2_Tareas/Login&Corroborar/App.js
@@ -4,6 +4,8 @@ var contraseña = document.forms['form']['password'];
 
 var us_error = document.getElementById('user_error');
 var pas_error = document.getElementById('password_error');
+var us_error1 = document.getElementById('user_error1');
+var pas_error1 = document.getElementById('password_error1');
 
 usuario.addEventListener('textInput', usuario_verificado);
 contraseña.addEventListener('textInput', contraseña_verificado);
@@ -13,14 +15,16 @@ function IniciarSesion()
     if (usuario.value.length < 9)
     {
         usuario.style.border = "1px solid red";
-        us_error.style.display = "block";
+        us_error1.style.display = 'none';
+        us_error.style.display = 'block';        
         usuario.focus();
         return false;
     }
     if (contraseña.value.length < 9)
     {
         contraseña.style.border = "1px solid red";
-        pas_error.style.display = "block";
+        pas_error1.style.display = 'none';
+        pas_error.style.display = 'block';        
         contraseña.focus();
         return false;
     }
@@ -30,8 +34,9 @@ function usuario_verificado()
 {
     if (usuario.value.length >= 8)
     {
-        usuario.style.border = "1px solid red";
-        us_error.style.display = 'none';
+        usuario.style.border = "1px solid rgb(144, 150, 155);";
+        us_error.style.display = "none";
+        us_error1.style.display = 'block';
         return true;
     }
 }
@@ -40,8 +45,9 @@ function contraseña_verificado()
 {
     if (contraseña.value.length >= 8)
     {
-        contraseña.style.border = "1px solid red";
-        pas_error.style.display = 'none';
+        contraseña.style.border = "1px solid rgb(144, 150, 155);";
+        pas_error.style.display = "none";
+        pas_error1.style.display = 'block';
         return true;
     }
 }
